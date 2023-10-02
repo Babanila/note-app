fa129759434154234fb45272e56ae314ac091262
Babajide Williams <babanila@yahoo.com>
2023-10-02 18:02:35 +0200

    Add prisma to server file

--- notes-app-server/index.ts
+++ notes-app-server/index.ts
@@ -1,17 +1,19 @@
+import { PrismaClient } from "@prisma/client";
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 
 dotenv.config();
-
-const app = express();
 const port = process.env.PORT || 5000;
+
+const prisma = new PrismaClient();
+const app = express();
 
 app.use(express.json());
 app.use(cors());
 
 app.get("/", async (req, res) => {
-  res.json({ message: "Ok" });
+  res.json({ status: "Ok" });
 });
 
 app.get("/api/notes", async (req, res) => {
@@ -19,7 +21,5 @@ app.get("/api/notes", async (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(
-    `⚡️⚡️⚡️[server]: Server is running at http://localhost:${port}`
-  );
+  console.log(`⚡️⚡️⚡️[server]: Server is running at http://localhost:${port}`);
 });
